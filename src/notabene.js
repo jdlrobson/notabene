669aa34a50ad4a08ff7e8f32be8f6732acfd625c
Jon Robson <jdlrobson@gmail.com>
2011-06-07 09:04:48 +0100

    pressing enter sets title
    bind the enter key to trigger a blur event and to set the title

--- src/notabene.js
+++ src/notabene.js
@@ -4,4 +4,9 @@ $(".note_title").blur(function(ev){
 	if($.trim(val).length > 0) {
 		$(".note_title").attr("disabled", true);
 	}
+}).keydown(function(ev) {
+	var code = ev.keyCode;
+	if(code === 13) {
+		$(".note_title").blur();
+	}
 });
