70c62137987a439818fce2984e72784bca34e2e7
Jon Robson <jdlrobson@gmail.com>
2011-06-08 18:15:33 +0100

    maintain tiddler object
    A tiddler object is created on startup.
    The title of which is manipulated when the title is changed.

--- src/notabene.js
+++ src/notabene.js
@@ -1,9 +1,13 @@
+var store =  new tiddlyweb.Store();
+var tempTitle = "untitled note " + Math.random();
+var note = new tiddlyweb.Tiddler(tempTitle);
 // on a blur event fix the title.
 $(".note_title").blur(function(ev){
 	var val = $(ev.target).val();
 	if($.trim(val).length > 0) {
 		$(".note_title").attr("disabled", true);
 	}
+	note.title = val;
 });
 
 // on clicking the "clear" button provide a blank note
