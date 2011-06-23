213808ce939d6a5793199cda96e936f380ccecc6
Jon Robson <jdlrobson@gmail.com>
2011-06-23 17:37:42 +0100

    fix breaking tests
    pass a tiddler object instead of a string

--- src/notabene.js
+++ src/notabene.js
@@ -132,7 +132,7 @@ function notes(container, options) {
 	function storeNote() {
 		note.fields.modified = new Date();
 		if(tempTitle && note.title != tempTitle) {
-			store.remove(tempTitle);
+			store.remove(new tiddlyweb.Tiddler(tempTitle, bag));
 		}
 		store.add(note);
 	}
