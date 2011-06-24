62b75bfc6e3d5aadf317124af2c052bc47b54294
Jon Robson <jdlrobson@gmail.com>
2011-06-24 16:36:34 +0100

    updated fixtures so that erroring ajax calls send dummy xhr objects
    also added a function setConnectionStatus which can be used by tests
    to simulate internet being turned off.

--- test/fixtures.js
+++ test/fixtures.js
@@ -1,12 +1,27 @@
+var no_connection_xhr = {
+	status: 0
+};
+
 var xhr = {
 	getResponseHeader: function() {
 		return "yes";
 	},
-	statusCode: 200
+	status: 200
+};
+
+var xhr404 = {
+	status: 404
+};
+
+var internet = true;
+function setConnectionStatus(status) {
+	internet = status;
 }
+
 var _oldAjax = $.ajax;
 $.ajax = function(options) {
 	var handlers = {
+		// tiddlers that live on the server and result in successful ajax
 		"/bags/bag/tiddlers/bar": function(options) {
 			var tid = { title: "bar", text: "The correct text",
 				modified: "201001131203",
@@ -16,21 +31,25 @@ $.ajax = function(options) {
 			};
 			options.success(tid, "tests", xhr);
 		},
+		// tiddlers that can be put to the server but never got or deleted
 		"/bags/bag/tiddlers/bar%20dum": function(options) {
 			if(options.type == "PUT") {
 				var tid = { title: "bar dum", fields: {}, text: "f" };
 				options.success("", 200, xhr);
 			}
-			options.error();
+			options.error(xhr404);
 		},
-		// a tiddler that is not on the server
+		// tiddlers that are not on the server
 		"/bags/test_public/tiddlers/Test": function(options) {
-			options.error();
+			options.error(xhr404);
 		},
 		"/bags/test_public/tiddlers/Test2": function(options) {
-			options.error();
+			options.error(xhr404);
 		}
 	};
+	if(!internet) {
+		options.error(no_connection_xhr);
+	}
 	if (options && handlers[options.url]) {
 		handlers[options.url](options);
 	} else {
