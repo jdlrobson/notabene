149dc28bb9642f079536f72d2498c68b27aff0ea
Jon Robson <jdlrobson@gmail.com>
2011-07-11 15:08:42 +0100

    describe how incomplete notes should work
    on the dashboard a list of notes that are in local storage and therefore
    unfinished (incomplete) should be listed.
    
    If no lists are there a message is displayed saying that there are none.

---
+++ test/test_dashboard.js
@@ -0,0 +1,43 @@
+var store;
+module('notabene dashboard - 2 tiddlers in store', {
+	setup: function() {
+		store = new tiddlyweb.Store();
+		var tid = new tiddlyweb.Tiddler("foo", new tiddlyweb.Bag("bar"));
+		var tid2 = new tiddlyweb.Tiddler("foo 2", new tiddlyweb.Bag("bar"));
+		dashboard(document.body, { bag: "bar", host: "/" });
+		store.add(tid);
+		store.add(tid2);
+		$("<ul id='incomplete' />").appendTo(document.body);
+		dashboard(document.body, { bag: "bar", host: "/" });
+	},
+	teardown: function() {
+		store = null;
+		localStorage.clear();
+		$("#incomplete").remove();
+	}
+});
+
+test('load incomplete notes', function() {
+	strictEqual($("#incomplete li").length, 2, "two notes are listed");
+	strictEqual($("#incomplete li a").length, 2, "both notes have links");
+	strictEqual($($("#incomplete li")[1]).text(), "foo 2", "the second note has the title foo 2")
+});
+
+module('notabene dashboard - empty store', {
+	setup: function() {
+		store = new tiddlyweb.Store();
+		$("<ul id='incomplete' />").appendTo(document.body);
+		dashboard(document.body, { bag: "bar", host: "/" });
+	},
+	teardown: function() {
+		store = null;
+		localStorage.clear();
+		$("#incomplete").remove();
+	}
+});
+
+test('load incomplete notes (empty store)', function() {
+	strictEqual($("#incomplete li").length, 1, "one item listed");
+	strictEqual($("#incomplete li").text(), "None.", "the user can see there is none.");
+	strictEqual($("#incomplete li a").length, 0, "no links");
+});
