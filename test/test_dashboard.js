62a22686c109ee94f33e0f0760aa2c02fab00c0c
Jon Robson <jdlrobson@gmail.com>
2011-07-14 11:18:06 +0100

    add tests to describe a list of recent links on dashboard
    the idea is for the dashboard to provide links to notes that have
    recently been created. It's not sure what "recent" means yet. It could
    be the last 10 or all notes created in the last X days etc..

--- test/test_dashboard.js
+++ test/test_dashboard.js
@@ -1,19 +1,21 @@
 var store;
 module('notabene dashboard - 2 tiddlers in store', {
 	setup: function() {
+		localStorage.setItem("takenote-recent-bar", '["foo", "bar", "test with spaces!>"]');
 		store = new tiddlyweb.Store();
 		var tid = new tiddlyweb.Tiddler("foo", new tiddlyweb.Bag("bar"));
 		var tid2 = new tiddlyweb.Tiddler("foo 2", new tiddlyweb.Bag("bar"));
 		dashboard(document.body, { bag: "bar", host: "/" });
 		store.add(tid);
 		store.add(tid2);
+		$("<ul id='recentnotes' />").appendTo(document.body);
 		$("<ul id='incomplete' />").appendTo(document.body);
 		dashboard(document.body, { bag: "bar", host: "/" });
 	},
 	teardown: function() {
 		store = null;
 		localStorage.clear();
-		$("#incomplete").remove();
+		$("#incomplete,#recentnotes").remove();
 	}
 });
 
@@ -25,16 +27,28 @@ test('load incomplete notes', function() {
 	strictEqual($("a", secondItem).attr("href"), "/takenote#!/tiddler/foo 2", "check the link points to the app");
 });
 
+test('print recent notes (there are recent notes)', function() {
+	strictEqual($("#recentnotes li").length, 3, "three items listed");
+	var links = $("#recentnotes li a");
+	var firstLink = $(links[0]);
+	var thirdLink = $(links[2]);
+	strictEqual(firstLink.text(), "bar", "the recent notes are sorted alphabetically");
+	strictEqual(thirdLink.text(), "test with spaces!>", "the recent notes are sorted alphabetically");
+	strictEqual(thirdLink.attr("href"), "/bags/bar/tiddlers/test%20with%20spaces!%3E",
+		"link correct and properly encoded");
+});
+
 module('notabene dashboard - empty store', {
 	setup: function() {
 		store = new tiddlyweb.Store();
 		$("<ul id='incomplete' />").appendTo(document.body);
+		$("<ul id='recentnotes' />").appendTo(document.body);
 		dashboard(document.body, { bag: "bar", host: "/" });
 	},
 	teardown: function() {
 		store = null;
 		localStorage.clear();
-		$("#incomplete").remove();
+		$("#incomplete,#recentnotes").remove();
 	}
 });
 
@@ -42,4 +56,10 @@ test('load incomplete notes (empty store)', function() {
 	strictEqual($("#incomplete li").length, 1, "one item listed");
 	strictEqual($("#incomplete li").text(), "None.", "the user can see there is none.");
 	strictEqual($("#incomplete li a").length, 0, "no links");
+});
+
+test('print recent notes (none)', function() {
+	strictEqual($("#recentnotes li").length, 1, "one item listed");
+	strictEqual($("#recentnotes li").text(), "None.", "the user can see there is none.");
+	strictEqual($("#recentnotes li a").length, 0, "no links");
 });
