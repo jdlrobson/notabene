373c54de3144a0e92b234a639869a863ee822210
Jon Robson <jdlrobson@gmail.com>
2011-08-02 18:02:09 +0100

    RELEASE 0.4.4

--- src/notabene.js
+++ src/notabene.js
@@ -1,6 +1,6 @@
 /*!
 |''Name''|notabene|
-|''Version''|0.4.2|
+|''Version''|0.4.4|
 |''License''|BSD (http://en.wikipedia.org/wiki/BSD_licenses)|
 |''Source''|https://github.com/jdlrobson/notabene/blob/master/src/notabene.js|
 !*/
