b0a6d2eda1e7a258afd6fe996a85a8e8279aa451
Jon Robson <jdlrobson@gmail.com>
2011-08-17 22:31:16 +0100

    add HtmlJavascript file
    this file should have been added in 59ba34919f1f8fda7423ad6ed8a31b4d0dfdd54a
    seems to have been omitted.

---
+++ src/htmljs-takenote.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+	var place = $("#container").length > 0 ? $("#container")[0] : document.body;
+	var space = window.location.host.split(".")[0]
+	var bag = $($(".bag")[0]).text() || space + "_public";
+	var title = $("#title").text();
+	// add edit link to notabene
+	$.ajax({ url: "/spaces/" + space + "/members",
+		success: function(r) {
+			if(r) {
+				if(bag == space + "_public") {
+					if($(".tiddler").length > 0) {
+						$("<a id='editLink' />").attr("href", "/takenote#!/quickedit/tiddler/" + title).
+							text("edit note").prependTo(place);
+					} else { // when viewing a collection add link to create new note in collection
+						$("<a id='editLink' />").attr("href", "/takenote").
+							text("take note").prependTo(place);
+					}
+				}
+			}
+		}
+	});
+});
