14c063e6d2c23f5780a45e08e26015465397f86c
Jon Robson <jdlrobson@gmail.com>
2011-08-23 15:05:56 +0100

    slight refactor
    hopefully this makes code more reusable.
    essentially the test is to see if a tiddler is in the html

--- src/htmljs-takenoteedit.js
+++ src/htmljs-takenoteedit.js
@@ -10,9 +10,10 @@ $(document).ready(function() {
 	// add edit link to notabene
 	$.ajax({ url: "/spaces/" + space + "/members",
 		success: function(r) {
+			var isTiddler = $(".tiddler").length > 0;
 			if(r) {
 				if(bag == space + "_public") {
-					if($(".tiddler").length > 0) {
+					if(isTiddler) {
 						$("<a id='editLink' />").attr("href", "/takenote#!/quickedit/tiddler/" + title).
 							text("edit note").prependTo(place);
 					} else { // when viewing a collection add link to create new note in collection
@@ -20,7 +21,7 @@ $(document).ready(function() {
 							text("take note").prependTo(place);
 					}
 				} else if(bag == space + "_private") {
-					if($(".tiddler").length > 0) {
+					if(isTiddler) {
 						$("<a id='editLink' />").attr("href", "/takenote#!/quickedit/bags/" + bag + "/tiddler/" + title).
 							text("edit note").prependTo(place);
 					}
